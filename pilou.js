91d42c7957ff1a3841b7d0984d245bf87c838d35
Guillaume Vincent <guillaume@oslab.fr>
2016-09-22 08:47:06 +0200

    use last version of es6-template-render

--- pilou.js
+++ pilou.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import generateTemplateString from 'es6-template-render';
+import render from 'es6-template-render';
 
 module.exports = function (resource, options = {}) {
   const defaultOptions = {
@@ -13,27 +13,27 @@ module.exports = function (resource, options = {}) {
   return {
     all(config = {}) {
       const templateUrl = config.url || defaultOptions.all;
-      const url = generateTemplateString(templateUrl)({resource});
+      const url = render(templateUrl, {resource});
       return axios.get(url, config);
     },
     create(json, config = {}) {
       const templateUrl = config.url || defaultOptions.create;
-      const url = generateTemplateString(templateUrl)({resource});
+      const url = render(templateUrl, {resource});
       return axios.post(url, json, config);
     },
     get(context, config = {}) {
       const templateUrl = config.url || defaultOptions.get;
-      const url = generateTemplateString(templateUrl)(Object.assign({resource}, context));
+      const url = render(templateUrl, Object.assign({resource}, context));
       return axios.get(url, config);
     },
     update(context, json, config = {}) {
       const templateUrl = config.url || defaultOptions.update;
-      const url = generateTemplateString(templateUrl)(Object.assign({resource}, context));
+      const url = render(templateUrl, Object.assign({resource}, context));
       return axios.put(url, json, config);
     },
     delete(context, config = {}) {
       const templateUrl = config.url || defaultOptions.delete;
-      const url = generateTemplateString(templateUrl)(Object.assign({resource}, context));
+      const url = render(templateUrl, Object.assign({resource}, context));
       return axios.delete(url, config);
     }
   };
